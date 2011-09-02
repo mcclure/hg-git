8280db545b3802f2a86cb816d395e63422a26c5a
mcc <andrew.mcclure@gmail.com>
2011-09-01 22:12:25 -0700

    virtualenv script loads dulwich and loads appropriate mercurial

--- tests/virtualenv-setup.py
+++ tests/virtualenv-setup.py
@@ -14,9 +14,12 @@ do_build = 'build' in args or 'run' in args
 
 versions = ['1.9.2', '1.9', '1.8.4', '1.8']
 
+dulwich_version = "dulwich-0.8.0"
+
 # I'm not doing any path correctness right now, this will probably break hard on Windows
 scratch_path = "ves_scratch/"
 mercurial_path = scratch_path + "hg"
+dulwich_path = scratch_path + "dulwich"
 baserev_path = "envs/"
 
 # At what path would an env with this hg tag be stored?
@@ -29,7 +32,15 @@ def ensure_mercurial():
         print("\t-- Downloading mercurial")
         subprocess.call(["mkdir", "-p", scratch_path])
         subprocess.call(["hg", "clone", "http://selenic.com/hg", mercurial_path])
-    pass
+        
+# Get a copy of the dulwich repository and move it to the single desired version.
+# Notice it would be easy to test for a range of dulwiches as is currently done with mercurial.
+def ensure_dulwich():
+    if not os.path.isdir(dulwich_path):
+        print("\t--Downloading dulwich")
+        subprocess.call(["mkdir", "-p", scratch_path])
+        subprocess.call(["hg", "clone", "git://git.samba.org/jelmer/dulwich.git", dulwich_path])
+        subprocess.call(["hg", "-R", dulwich_path, "up", dulwich_version])
     
 # Set up an environment for the given version of hg.
 def ensure_env(version):
@@ -37,8 +48,13 @@ def ensure_env(version):
     if not os.path.isdir(path):
         print("\t-- Setting up version %s" % version)
         ensure_mercurial()
+        ensure_dulwich()
         subprocess.call(["mkdir", "-p", baserev_path])
-        # TODO
+        print("\t-- Making virtualenv" % version)
+        subprocess.call(["virtualenv", path])
+        print("\t-- Loading appropriate mercurial" % version)
+        subprocess.call(["hg", "-R", mercurial_path, "up", version])
+        # TODO: Install mercurial and Dulwich to new env.
 
 # Act
 if do_build:
